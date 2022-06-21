cf9ed9aa89706b1fe76fd008baf081601d39c667
anastasia <piganova.nastya@gmail.com>
2022-06-21 18:21:45 +0300

    add new files

--- test_google.py
+++ test_google.py
@@ -1,10 +1,12 @@
 import pytest
-from selene.support.shared import browser
-from selene import be, have
-
+from browser import browser
 
 
 @pytest.fixture(scope='session', autouse=True)
 def browser_size():
     browser.open('data:')
     browser.driver.set_window_size(width=1920, height=1080)
+
+    @pytest.fixture()
+    def open_browser():
+        browser.open("https://www.google.com/")
