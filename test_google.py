518f0faf356a018567ca3ca41f4ac5d45d76817a
anastasia <piganova.nastya@gmail.com>
2022-06-24 15:52:32 +0300

    add new files

--- test_google.py
+++ test_google.py
@@ -1,13 +1,2 @@
-import pytest
-import tests_negative
-from browser import browser
 
-
-@pytest.fixture(scope='session', autouse=True)
-def browser_size():
-    browser.open('data:')
-    browser.driver.set_window_size(width=1920, height=1080)
-
-    @pytest.fixture()
-    def open_browser():
-        browser.open("https://www.google.com/")
+def test_first():
